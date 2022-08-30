c95d59f3fc77f464655671b650757316d51ccc86
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-08-30 14:37:49 -0500

    Update ESP32CAM-MQTT-MLX90614-MAX30100-JSON.ino

--- ESP32/ESP32CAM-MQTT-MLX90614-MAX30100-JSON/ESP32CAM-MQTT-MLX90614-MAX30100-JSON.ino
+++ ESP32/ESP32CAM-MQTT-MLX90614-MAX30100-JSON/ESP32CAM-MQTT-MLX90614-MAX30100-JSON.ino
@@ -82,7 +82,7 @@ void setup() {
   pinMode (ledPin, OUTPUT);
   pinMode (ledPin2, OUTPUT);
   digitalWrite (ledPin, HIGH);
-  digitalWrite (ledPin2, HIGH);
+  digitalWrite (ledPin2, LOW);
 
   //Pines Indicadores de funcionamiento del MAX30100
   //pinMode(pulseLED, OUTPUT);
@@ -186,7 +186,7 @@ void loop() {
 
       //send samples and calculation result to terminal program through UART
 
-      Serial.print(F(", HR="));
+      Serial.print(F("HR="));
       Serial.print(heartRate, DEC);
 
       Serial.print(F(", HRvalid="));
@@ -209,7 +209,7 @@ void loop() {
     timeLastMQTT = timeNow; // Actualización de seguimiento de tiempo
 
     //Se construye el string correspondiente al JSON que contiene 3 variables
-    String json = "{\"hr\"=" + String (heartRate) + ",\"spo2\":" + String (spo2) +"}";
+    String json = "{\"hr\"=" + String (heartRate) + ",\"hrv\":" + String (validHeartRate) + ",\"spo2\":" + String (spo2) + ",\"spo2v\":"+ String (validSPO2) + "}";
     Serial.println(json); // Se imprime en monitor solo para poder visualizar que el string esta correctamente creado
     int str_len = json.length() + 1;//Se calcula la longitud del string
     char char_array[str_len];//Se crea un arreglo de caracteres de dicha longitud
